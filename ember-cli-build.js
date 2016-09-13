9ba98b64a91e000b58aff3c66904df5664dff7eb
Christopher Soden <karavol27@gmail.com>
2016-09-13 12:35:21 -0600

    [WIP] install js-cookie and add import to ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/js-cookie/src/js.cookie.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
